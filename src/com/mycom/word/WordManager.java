4767404dc654eac7720acd8ee7f8825d5f477954
highlyko17 <highlyko17@gmail.com>
2022-09-02 11:13:50 +0900

    third commit

--- src/com/mycom/word/WordManager.java
+++ src/com/mycom/word/WordManager.java
@@ -12,7 +12,7 @@ public class WordManager {
 	
 	public int selectMenu() {
 		System.out.print("*** 영단어 마스터 ***\n"
-				+ "**********************\n"
+				+ "\n**********************\n"
 				+ "1. 모든 단어 보기\n"
 				+ "2. 수준별 단어 보기\n"
 				+ "3. 단어 검색\n"

a1890e54447a692573bdacf6c7ee4884b5673840
highlyko17 <highlyko17@gmail.com>
2022-09-07 23:03:04 +0900

    add update and delete function

--- src/com/mycom/word/WordCRUD.java
+++ src/com/mycom/word/WordCRUD.java
@@ -28,7 +28,7 @@ public class WordCRUD implements ICRUD {
 		return new Word(0, level, word, meaning);
 	}
 	
-	public void addWord() {
+	public void addItem() {
 		Word one = (Word)add();
 		list.add(one);
 		System.out.println("\n새 단어가 단어장에 추가되었습니다.\n");
@@ -60,5 +60,55 @@ public class WordCRUD implements ICRUD {
 			System.out.println(list.get(i).toString());
 		}
 		System.out.println("----------------------------------\n");
+	}
+	
+	public ArrayList<Integer> listAll(String keyword) {
+		ArrayList<Integer> idlist = new ArrayList<>();
+		int j = 0;
+		
+		System.out.println("\n----------------------------------");
+		for(int i = 0; i < list.size(); i++) {
+			String word = list.get(i).getWord();
+			if(!word.contains(keyword)) continue;
+			System.out.print((j + 1) + " ");
+			System.out.println(list.get(i).toString());
+			idlist.add(i);
+			j++;
+		}
+		System.out.println("----------------------------------\n");
+		
+		return idlist;
+	}
+
+	public void updateItem() {
+		System.out.print("=> 수정할 단어 검색 : ");
+		String keyword = s.next(); // 공백을 허용하지 않기 위해서 next()
+		ArrayList<Integer> idlist = this.listAll(keyword); 
+		
+		System.out.print("=> 수정할 번호 선택 : ");
+		int id = s.nextInt();
+		s.nextLine(); // 번호 입력 받고 친 엔터가 뜻 입력에 들어가버려서 이 줄 추
+		System.out.print("=> 뜻 입력 : ");
+		String meaning = s.nextLine(); // 공백 포함에서 받아야하니 nextLine()
+		Word word = list.get(idlist.get(id - 1));
+		word.setMeaning(meaning);
+		System.out.println("단어가 수정되었습니다.");
+	}
+
+	public void deleteItem() {
+		System.out.print("=> 삭제할 단어 검색 : ");
+		String keyword = s.next(); // 공백을 허용하지 않기 위해서 next()
+		ArrayList<Integer> idlist = this.listAll(keyword); 
+		
+		System.out.print("=> 삭제할 번호 선택 : ");
+		int id = s.nextInt();
+		s.nextLine(); // 번호 입력 받고 친 엔터가 뜻 입력에 들어가버려서 이 줄 추
+		System.out.print("=> 정말로 삭제하실래요? (Y/N) ");
+		String ans = s.next(); // 한 단어로 받아야하니 next
+		if(ans.equalsIgnoreCase("y")) {
+			list.remove((int)idlist.get(id - 1)); // remove 함수는 파라미터에 객체를 넣던지 정수형으로 몇 번째 인덱스의 객체를 삭제할 것인지 넣어야 함.
+			System.out.println("단어가 삭제되었습니다.");
+		} else
+			System.out.println("취소되었습니다.");
 	}
 }
